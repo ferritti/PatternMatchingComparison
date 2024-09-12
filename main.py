93db0d1b30c8c585fd4273ac7fe7f040c151f393
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2024-09-12 17:53:19 +0200

    Implement KPM Algorithm for string pattern matching

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-
+import numpy as np
 
 def naive_string_matcher(T, P):
     n = len(T)
     m = len(P)
 
-    for s in range(n - m + 1):  #Scorre il testo fino all'indice n - m
-        if T[s:s + m] == P:  #Controlla se il pattern coincide con la sottostringa del testo
+    for s in range(n - m + 1):
+        if T[s:s + m] == P:
             print(f"Occorrenza del pattern con spostamento {s}")
 
+
+def kmp_matcher(T, P):
+    n = len(T)
+    m = len(P)
+    pi = compute_prefix_function(P, m)
+    q = 0
+
+    for i in range(n):
+        while q > 0 and P[q] != T[i]:
+            q = pi[q]
+        if P[q] == T[i]:
+            q += 1
+        if q == m:
+            print(f"Occorrenza del pattern con spostamento {i - m + 1}")
+            q = pi[q - 1]
+
+def compute_prefix_function(P, m):
+    pi = np.zeros(m,dtype=int)
+    k = 0
+
+    for q in range(1, m):
+        while k > 0 and P[k] != P[q]:
+            k = pi[k - 1]
+        if P[k] == P[q]:
+            k += 1
+        pi[q] = k
+
+    return pi
